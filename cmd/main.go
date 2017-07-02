11a0b1c46de2dca0808e2786b4e22cf75c411ce1
sky0621 <sky14work@gmail.com>
2017-07-02 23:29:53 +0900

    zap

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,19 @@ import (
 	"flag"
 	"path/filepath"
 
-	"github.com/Sirupsen/logrus"
 	ag "github.com/sky0621/go-testcode-autogen"
 	"github.com/sky0621/go-testcode-autogen/config"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	logger.Info("START")
+
 	target := flag.String("target", "../_example/sampleproject", "Parse Target")
 	cfg := flag.String("config", "../config/config.toml", "Config File")
 	flag.Parse()
@@ -17,8 +24,9 @@ func main() {
 	config.ReadConfig(*cfg)
 	ag.Cfg = config.NewConfig()
 
-	err := filepath.Walk(*target, ag.Apply)
+	err = filepath.Walk(*target, ag.Apply)
 	if err != nil {
-		logrus.Error(err)
+		logger.Error("ABEND", zap.Error(err))
 	}
+	logger.Info("END")
 }
